7612d1d9d68fbf207490dd080e5f002016f0f88d
jinyoung <jinyoung@sandboxnetwork.net>
2022-07-09 23:21:45 +0900

    fix: 크롬 드라이버를 드라이버 매니저 서비스 이용하도록 수정(기존 세션 설정 depecrete 예정)

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-decoder_book_rubato = "http://decoder.kr/book-rubato/"
-driver = webdriver.Chrome(decoder_book_rubato)
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-xpath = "//html/body/div[4]/table/tbody/tr/td[3]/div/div/div[4]/div/button[23]"
-driver.find_element_by_xpath(xpath).click()
+
+def create_decoder_book_instance(chrome_driver: webdriver.Chrome):
+    decoder_book_rubato = "http://decoder.kr/book-rubato/"
+    chrome_driver.get(decoder_book_rubato)
+    chrome_driver.quit()
+    return chrome_driver
+
+
+create_decoder_book_instance(driver)
+
+# xpath = "//html/body/div[4]/table/tbody/tr/td[3]/div/div/div[4]/div/button[23]"
+# driver.find_element_by_xpath(xpath).click()
