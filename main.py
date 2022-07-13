06a1cbafa5b4ba1cdd6f05d2e001f56e22c6a93d
dharana <dharana7723@gmail.com>
2022-07-14 02:33:51 +0900

    feat: 캘린더 날짜 순회 기능 추가

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 from webdriver_manager.chrome import ChromeDriverManager
+import time
 
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
@@ -14,8 +15,25 @@ def create_decoder_book_instance(chrome_driver: webdriver.Chrome):
     month, year = get_month_and_year(chrome_driver)
 
     time_screen = find_time_screen(chrome_driver)
-    buttons = time_screen.find_elements(By.CSS_SELECTOR, value="button")
-    can_book_buttons = get_can_book_buttons(buttons)
+    time_buttons = time_screen.find_elements(By.CSS_SELECTOR, value="button")
+    can_book_time_buttons = get_can_book_buttons(time_buttons)
+
+    calender = find_calender(chrome_driver)
+    next_month_calender_button = calender.find_element(by=By.CLASS_NAME, value="picker__nav--next")
+
+    current_date = -1
+    end_date = 31
+
+    while True:
+        picker_table = calender.find_element(by=By.CLASS_NAME, value="picker__table")
+        table_dates = picker_table.find_elements(by=By.CSS_SELECTOR, value="td")
+        current_table_date = get_next_clickable_date(table_dates, current_date)
+        if current_table_date is None:
+            break
+        current_div_date = current_table_date.find_element(by=By.CSS_SELECTOR, value="div")
+        current_date = int(current_div_date.text)
+        current_table_date.click()
+        print(current_date)
 
     chrome_driver.quit()
     return chrome_driver
@@ -41,6 +59,27 @@ def get_can_book_buttons(buttons: list):
             continue
         can_book_buttons.append(button)
     return can_book_buttons
+
+
+def find_calender(chrome_driver: webdriver.Chrome):
+    picker_holder = chrome_driver.find_element(by=By.CLASS_NAME, value="picker__holder")
+    return picker_holder
+
+
+def get_next_clickable_date(table_dates: list, current_date: int = -1):
+    for table_date in table_dates:
+        div_date = table_date.find_element(by=By.CSS_SELECTOR, value="div")
+
+        can_be_booked = div_date.get_attribute("aria-disabled")
+        aria_selected = div_date.get_attribute("aria-selected")
+        aria_active_descendant = div_date.get_attribute("aria-activedescendant")
+
+        if can_be_booked is None and (aria_selected != "true" and aria_active_descendant != "true"):
+            if current_date < int(div_date.text):
+                current_date = int(div_date.text)
+                return table_date
+        if table_date.text == "31":
+            return None
 
 
 create_decoder_book_instance(driver)
